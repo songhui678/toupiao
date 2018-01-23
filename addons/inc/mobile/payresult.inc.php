d38271d41df4387215c9329f5fed545814fc5b0e
songhui1 <songhui@daojia.com.cn>
2018-01-23 18:17:53 +0800

    123

---
+++ addons/inc/mobile/payresult.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 钻石投票-奖品
+ *
+ * @author weizan
+ * @url http://weizan/
+ */
+
+defined('IN_IA') or exit('Access Denied');
+global $_W,$_GPC;
+
+
+$_W['page']['sitename']="支付提示";
+include $this->template('payresult');
