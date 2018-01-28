d5b90f5e7e6d801db529131e7c8802f4cad344fa
songhui678 <songhui678@gmail.com>
2018-01-28 12:09:07 +0800

    666

--- addons/tyzm_diamondvote/inc/mobile/voteuser.inc.php
+++ addons/tyzm_diamondvote/inc/mobile/voteuser.inc.php
@@ -9,10 +9,8 @@
 defined('IN_IA') or exit('Access Denied');
 global $_W, $_GPC;
 $time = time();
-$voteuserList = pdo_fetch("SELECT * FROM " . tablename($this->tablevoteuser) . " WHERE rid=:rid AND status=:status ", array(
-	':rid' => 13,
-	':status' => 0,
-));
+$voteuserList = pdo_fetch("SELECT * FROM " . tablename($this->tablevoteuser) . " WHERE rid=13 AND status=0 ");
+var_dump($voteuserList);exit;
 if (!empty($voteuserList)) {
 	foreach ($voteuserList as $voteuser) {
 		$diffTime = $time - $voteuser['createtime'];
