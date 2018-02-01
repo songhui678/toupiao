8614e719daba6c3efc52dc4c9365f238eb68bd68
songhui1 <songhui@daojia.com.cn>
2018-02-01 15:23:45 +0800

    432432

--- addons/tyzm_diamondvote/inc/mobile/gift.inc.php
+++ addons/tyzm_diamondvote/inc/mobile/gift.inc.php
@@ -81,7 +81,7 @@ if ($ty['ispost']) {
 			exit(json_encode(array('status' => '0', 'msg' => "最多能送" . $reply['everyonediamond'] . "元礼物，给其他人点机会吧！:-D")));
 		}
 	}
-	$tid = $userinfo['openid'];
+	$tid = md5($userinfo['openid']);
 	$params = array(
 		'tid' => $tid,
 		'ordersn' => $tid,
